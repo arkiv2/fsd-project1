fbf6fe6835abb111aafb5b15bf04b2ee028e6a62
Arki Valencia II <arvalencia@gobx.adnu.edu.ph>
2016-03-22 10:30:31 -0400

    integrated passed test scripts to main code logic

--- boom_box.py
+++ boom_box.py
@@ -1,1 +1,15 @@
 import favorite
+import urllib
+import json
+
+def getIMDBInfo(imdb_code): #Movie's IMDB Code
+    connection = urllib.urlopen("http://www.omdbapi.com/?i="
+                         +imdb_code+
+                         "&plot=full&r=json") #Query OMDBAPI for IMDB info
+    movie = json.loads(connection.read()) #Convert JSON data to objects 
+    connection.close()
+    return movie    #Returns Serialized JSON
+
+toy_story_3 = favorite.Movie(getIMDBInfo("tt0435761")) #tt0435761 is toy story 3's IMDB Code
+
+print(toy_story_3.plot)
--- favorite.py
+++ favorite.py
@@ -1,1 +1,12 @@
 class Movie():
+    def __init__(self, movie_info):
+        self.title = movie_info['Title']
+        self.plot = movie_info['Plot']
+        self.year = movie_info['Year']
+        self.genre = movie_info['Genre']
+        self.poster = movie_info['Poster']
+        self.trailer_url = movie_info['Poster']
+        self.released = movie_info['Released']
+        self.writer = movie_info['Writer']
+        self.actors = movie_info['Actors']
+        self.rating = movie_info['imdbRating']
