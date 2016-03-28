21fb1ace91b3684128baaffd761f6760b443ccf4
Arki Valencia II <arvalencia@gbox.adnu.edu.ph>
2016-03-28 20:27:26 +0800

    added comments

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from fresh_tomatoes import open_movies_page
 from boom_box import getIMDBInfo
 from media import Movie
 
-movies = []
+movies = [] #Initialize list to be appended when movies are created
 
-
+''' 
+  Declare movies using the syntax 
+  <movie_name> = Movie(<movie_list>, getIMDBInfo("<movie_imdb_id"), "<movie_youtube_trailer_url>")
+'''
 dead_pool = Movie(movies, getIMDBInfo("tt1431045"),
                           "https://www.youtube.com/watch?v=ZIM1HydF9UA")
 
@@ -17,5 +20,9 @@ law_abiding_citizen = Movie(movies, getIMDBInfo("tt1197624"),
 interstellar = Movie(movies, getIMDBInfo("tt0816692"),
                      "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
 
+'''
+  Call the open_movies_page function with the movies list as parameter
+  from fresh_tomatoes module to render movies in an html file
+'''
+open_movies_page(movies) 
 
-open_movies_page(movies)
