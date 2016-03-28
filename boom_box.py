0fdcd2c0b3728dc923ce41c7d1b5b32bbdcecb7c
Arki Valencia II <arvalencia@gbox.adnu.edu.ph>
2016-03-28 20:36:52 +0800

    added comments

--- boom_box.py
+++ boom_box.py
@@ -1,3 +1,12 @@
+"""boom_box.py: module to define a function that communicated with
+    an external API to query movie info from IMDB"""
+
+__author__ = "Archimedes Valencia II"
+
+__version__ = "1.0.3"
+__email__ = "arvalencia@gbox.adnu.edu.ph"
+__status__ = "Production"
+
 from fresh_tomatoes import open_movies_page
 from media import Movie
 import urllib
